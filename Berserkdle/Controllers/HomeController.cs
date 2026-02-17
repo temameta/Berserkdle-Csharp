a9364552b8f1cb1674bd2eac59e0077170407b8f
temameta <sezemov2@gmail.com>
2026-02-17 15:29:21 +0300

    feat: persons and creating views added

--- Berserkdle/Controllers/HomeController.cs
+++ Berserkdle/Controllers/HomeController.cs
@@ -1,19 +1,35 @@
 using System.Diagnostics;
+using Berserkdle.Data;
 using Microsoft.AspNetCore.Mvc;
 using Berserkdle.Models;
 
 namespace Berserkdle.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger, BerserkdleDbContext context)
+    : Controller
 {
+
     public IActionResult Index()
     {
         return View();
     }
 
+    public IActionResult Persons()
+    {
+        var allPersons = context.Persons.ToList();
+        return View(allPersons);
+    }
+
     public IActionResult CreateEditPerson()
     {
         return View();
+    }
+
+    public IActionResult CreateEditPersonForm(Person model)
+    {
+        context.Persons.Add(model);
+        context.SaveChanges();
+        return RedirectToAction("Persons");
     }
 
     public IActionResult Privacy()
