4181ba3d9001760eb58fab7b58f318ddbbe6167f
Tema <sezemov2@gmail.com>
2026-02-18 17:22:27 +0300

    feat: game controller

---
+++ Berserkdle/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Berserkdle.Controllers;
+
+public class GameController : Controller
+{
+    public IActionResult Main()
+    {
+        return View();
+    }
+}
