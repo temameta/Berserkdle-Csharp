e9247da04f039fd94d560d1e383b8ec0198687dd
temameta <sezemov2@gmail.com>
2026-02-24 12:34:53 +0300

    refactor: GameView.cs renamed into GameViewModel.cs, added new fields

--- Models/GameView.cs
+++
@@ -1,9 +0,0 @@
-﻿using Berserkdle.Data;
-
-namespace Berserkdle.Models;
-
-public class GameView(List<string> AllNames, List<Person> GuessedPersons)
-{
-    public List<string> AllNames { get; set; }
-    public List<Person> GuessedPersons { get; set; }
-}
---
+++ Models/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Berserkdle.Data;
+
+namespace Berserkdle.Models;
+
+public class GameViewModel
+{
+    public List<string> AllNames { get; set; } = new();
+    public List<PersonGuessResult> Guesses { get; set; } = new();
+    public int Attempts { get; set; }
+    public bool GameWon { get; set; }
+    public string MysteryPerson { get; set; }
+    public string ErrorMessage { get; set; }
+}
