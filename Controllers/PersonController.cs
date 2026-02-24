3de4acc54b6a11b656b3b0bcac0ba196d7cbaa26
temameta <sezemov2@gmail.com>
2026-02-24 13:35:42 +0300

    feat: added parsing groups and weapons from string to list of strings

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -24,9 +24,29 @@ public class PersonController(BerserkdleDbContext context) : Controller
         return View();
     }
 
-    public IActionResult CreateEditPersonForm(Person model)
+    public IActionResult CreateEditPersonForm(CreatePerson model)
     {
-        context.Persons.Add(model);
+        var parsedWeapons = new List<string>();
+        var parsedGroups = new List<string>();
+        foreach (var weapon in model.Weapons.Split(","))
+        {
+            parsedWeapons.Add(weapon.Trim());
+        }
+        foreach (var group in model.Groups.Split(","))
+        {
+            parsedGroups.Add(group.Trim());
+        }
+
+        var newPerson = new Person
+        {
+            Name = model.Name,
+            Gender = model.Gender,
+            Species = model.Species,
+            FirstArc = model.FirstArc,
+            Groups = parsedGroups,
+            Weapons = parsedWeapons
+        };
+        context.Persons.Add(newPerson);
         context.SaveChanges();
         return RedirectToAction("Persons");
     }
---
+++ Models/CreatePerson.cs
@@ -0,0 +1,11 @@
+﻿namespace Berserkdle.Models;
+
+public class CreatePerson
+{
+    public string? Name { get; set; }
+    public string? Gender { get; set; }
+    public string? FirstArc { get; set; }
+    public string? Species { get; set; }
+    public string? Weapons { get; set; }
+    public string? Groups { get; set; }
+}
