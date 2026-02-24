7ef069ce726940060b11641a0acd2a94de4981cf
temameta <sezemov2@gmail.com>
2026-02-24 12:35:08 +0300

    feat: new model PersonGuessResult.cs

---
+++ Models/PersonGuessResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Berserkdle.Models;
+
+public class PersonGuessResult
+{
+    public string Name { get; set; }
+    public bool NameMatches { get; set; }
+    public string Gender { get; set; }
+    public bool GenderMatches { get; set; }
+    public string Species { get; set; }
+    public bool SpeciesMatches { get; set; }
+    public string FirstArc { get; set; }
+    public bool FirstArcMatches { get; set; }
+    public List<string> Weapons { get; set; } = new();
+    public bool AnyWeaponMatches { get; set; }
+    public List<string> Groups { get; set; } = new();
+    public bool AnyGroupMatches { get; set; }
+}
